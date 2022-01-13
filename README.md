ab5c13aa30b9353f540e874af6e55f03e28c493f
Cole Shepherd <cole@recroom.com>
2022-01-12 20:56:25 -0800

    Added SSL to the server.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Tech-Startup-Template
+
+## Running the server
+
+If on Windows:
+
+1. Install [vcpkg](https://vcpkg.io/en/getting-started.html)
+2. Run `vcpkg install openssl:x64-windows-static-md`
+
+1. `cd` into `server`
+2. run `cargo run`
+3. Navigate to [https://localhost:8080/](https://localhost:8080/) in your browser.
+
+NOTE: The SSL key was generated with the following command: `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -sha256 -subj '/CN=localhost'`
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,13 +17,21 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // load SSL keys
+    let mut builder =
+        SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8080")?
+    .bind_openssl("127.0.0.1:8080", builder)?
     .run()
     .await
 }
